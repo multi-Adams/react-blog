87e8f5821fd3c1ecf07d00a1b7ed8f4f90120f0a
Adams Lee <officialman776@gmail.com>
2021-05-01 23:13:04 +0100

    added the delete feature

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # React Blog
+
+## Lists blogs using a flie-based DB by JSON-Server
+
+When i learn TailWindCSS i'll style it. Promise!
--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "../hooks/useFetch";
 
 function BlogDetails() {
   const { id } = useParams();
+  const history = useHistory();
+
   const { data: blog, error, isLoading } = useFetch(
     `http://localhost:5000/blogs/${id}`
   );
+
+  const handleClick = () => {
+    fetch(`http://localhost:5000/blogs/${blog.id}`, { method: "DELETE" }).then(
+      () => {
+        history.push("/");
+      }
+    );
+  };
   return (
     <div>
       {error && <h3>{error}</h3>}
@@ -17,6 +27,7 @@ function BlogDetails() {
           <br /> <p>{blog.body}</p> <br /> <p>Written By {blog.author}</p>
         </article>
       )}
+      <button onClick={handleClick}>delete blog</button>
     </div>
   );
 }
